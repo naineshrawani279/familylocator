618296c547b1a845420f8a476abb633d328cb976
nainesh279 <31571436+nainesh279@users.noreply.github.com>
2017-09-02 22:50:26 +0530

    Create login.php

---
+++ login.php
@@ -0,0 +1,22 @@
+<?php
+
+include_once 'dbconnect.php';
+
+//check if form is submitted
+if (isset($_POST['login'])) {
+
+    $email = mysqli_real_escape_string($con, $_POST['email']);
+    $password = mysqli_real_escape_string($con, $_POST['password']);
+    $result = mysqli_query($con, "SELECT * FROM users WHERE email = '" . $email. "' and password = '" . md5($password) . "'");
+
+    if ($row = mysqli_fetch_array($result)) {
+        $_SESSION['usr_id'] = $row['id'];
+        $_SESSION['usr_name'] = $row['name'];
+        $successmsg = "Succesfully Logged in";
+        echo $successmsg;
+    } else {
+        $errormsg = "Incorrect Email or Password!";
+        echo $errormsg;
+    }
+}
+?>
